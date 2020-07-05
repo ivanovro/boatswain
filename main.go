5412b5cc7e2914cb914e41abf8b51e82a3fd291e
Roman Ivanov <roman.ivanov.7@gmail.com>
2020-07-05 22:31:43 +0200

    post/delete handling

--- main.go
+++ main.go
@@ -27,35 +27,47 @@ func handleHttpRequests(client *versioned.Clientset) {
 func rootHandler(client *versioned.Clientset) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
-
 		chartName := r.RequestURI[1:]
-
 		switch r.Method {
-		case "GET":
+		case "POST":
 			helmRequest := makeHelmRequest(chartName)
-			req, err := captainRequestCreate(client, helmRequest)
+			_, err := captainRequestCreate(client, helmRequest)
 			if err != nil {
-				panic(err)
+				replyError(w, err)
+			} else {
+				confirmRequest(w, chartName, "installation")
 			}
-			w.WriteHeader(http.StatusOK)
-			status := fmt.Sprintf(`{"status": "%s installation in progress"}`, req.Status.Notes)
-			w.Write([]byte(status))
 		case "DELETE":
-			w.WriteHeader(http.StatusCreated)
-			w.Write([]byte(`{"message": "POST method requested"}`))
+			err := captainRequestDelete(client, chartName)
+			if err != nil {
+				replyError(w, err)
+			} else {
+				confirmRequest(w, chartName, "deletion")
+			}
 		default:
 			w.WriteHeader(http.StatusNotFound)
-			w.Write([]byte(`{"message": "Can't find method requested"}`))
+			_, _ = w.Write([]byte(`{"message": "Can't find method requested"}`))
 		}
 	}
 }
 
+func confirmRequest(w http.ResponseWriter, chartName string, operation string) {
+	w.WriteHeader(http.StatusOK)
+	status := fmt.Sprintf(`{"status": "'%s' %s in progress"}`, chartName, operation)
+	_, _ = w.Write([]byte(status))
+}
+
+func replyError(w http.ResponseWriter, err error) {
+	w.WriteHeader(http.StatusInternalServerError)
+	status := fmt.Sprintf(`{"status": "%s"}`, err)
+	_, _ = w.Write([]byte(status))
+}
+
 func captainRequestCreate(c *versioned.Clientset, helmRequest *v1alpha1.HelmRequest) (*v1alpha1.HelmRequest, error) {
-	request, err := c.AppV1alpha1().HelmRequests("default").Create(helmRequest)
-	if err != nil {
-		panic(err)
-	}
-	return request, err
+	return c.AppV1alpha1().HelmRequests("default").Create(helmRequest)
+}
+func captainRequestDelete(c *versioned.Clientset, chartName string) error {
+	return c.AppV1alpha1().HelmRequests("default").Delete(chartName, nil)
 }
 
 func makeHelmRequest(chartName string) *v1alpha1.HelmRequest {
